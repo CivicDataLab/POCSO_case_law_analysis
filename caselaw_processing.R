411580e3569e4bed568892b176dda99c8a4abc65
apoorv74 <apoorv7491@gmail.com>
2019-02-28 18:33:01 +0530

    Add coordinates and geoJson for police stations

--- caselaw_processing.R
+++ caselaw_processing.R
@@ -2,6 +2,8 @@
 
 library(tidyverse)
 library(progress)
+library(ggmap)
+library(geojsonio)
 
 sample_data_dir <- "datasets/Maharashtra/"
 
@@ -18,9 +20,11 @@ for (i in 1:length(all_files)) {
   x <- jsonlite::read_json(all_files[[i]])
 
   cino <- ifelse(is.null(x$cino),"",x$cino)
+  
   ## List of variables from Vivek
   
   fil_year <- ifelse(is.null(x$fil_year),"",as.character(x$fil_year))
+  disp_nature <- ifelse(is.null(x$disp_nature),"",as.character(x$disp_nature))
   jcode <- ifelse(is.null(x$jcode),"",as.character(x$jcode))
   date_of_decision <- ifelse(is.null(x$date_of_decision),"",as.character(x$date_of_decision))
   reg_year <- ifelse(is.null(x$reg_year),"",as.character(x$reg_year))
@@ -58,6 +62,8 @@ for (i in 1:length(all_files)) {
 
   case_fir_details[[i]] <-
     data.frame(
+      cino,
+      disp_nature,
       fil_year,
       jcode,
       date_of_decision,
@@ -99,45 +105,68 @@ for (i in 1:length(all_files)) {
   
   case_history <- ifelse(is.null(x$historyOfCaseHearing),"",x$historyOfCaseHearing)
   if(case_history != ""){
-   case_history_table <- case_history %>% read_html() %>% html_table() 
+   case_history_table <- case_history %>% read_html() %>% html_table()
    case_history_table <- case_history_table[[1]]
    case_history_table$cino <- cino
   } else {
     case_history_table <- data.frame("cino"=cino,"Judge"="", "Business On Date" = "", "Hearing Date" = "",check.names=FALSE)
   }
-  
+
   case_history_list[[i]] <- case_history_table
   
   # View progress
+
   pb$tick()
 }
 
 # Transforming case meta variables
+
 case_fir_df <- dplyr::bind_rows(case_fir_details)
 case_fir_df$police_station <- stringr::str_replace_all(case_fir_df$fir_details, pattern = "\\^", replacement = "")
 case_fir_df$police_station <- stringr::str_replace_all(case_fir_df$police_station, pattern = "[:digit:]+", replacement = "")
 
 #Transforming case history variables
+
 case_history_df <- dplyr::bind_rows(case_history_list)
 
-# Exporting datasets
-data.table::fwrite(case_fir_df, "datasets/Maharashtra/processed/mh_pocso_01.csv")
-data.table::fwrite(case_history_df, "datasets/Maharashtra/processed/mh_pocso_case_history_02.csv")
+## Find unique police station across the database and assign unique ID's
 
+police_station_id <- data.frame(police_station = unique(case_fir_df$police_station))
+police_station_id$police_station_id <- 1:nrow(police_station_id)
 
+case_fir_df <- dplyr::left_join(case_fir_df, police_station_id, by="police_station")
 
-
-
-
-
-
-# To get the co-ordinates
+## Using google geocoding API to get police station coordinates
 
 get_coordinates <- function(police_station_name) {
-  police_station_name <- paste0(police_station_name," India")
-  police_station_name <- stringr::str_replace_all(police_station_name,pattern = " ",replacement = "%20")
-  geocode_url <- glue::glue('https://api.opencagedata.com/geocode/v1/json?q={police_station_name}%2C%20India&key=22a580c8eaf74006ae634edfdd4d514a&language=en&pretty=1&no_annotations=1')
-  geocode_request <- httr::VERB(verb = 'GET',geocode_url)
-  geocode_response <- httr::content(geocode_request)
-  
+  police_station_name <- paste0(police_station_name,", Maharashtra, India")
+  police_station_coords <- ggmap::geocode(police_station_name, output = "latlon") %>% data.frame()
+  return(police_station_coords)
 }
+
+## Generate a master dataset of police stations 
+
+police_station_coord_list <- lapply(police_station_id$police_station, get_coordinates)
+police_station_coord_df <- dplyr::bind_rows(police_station_coord_list)
+police_station_coord_df$police_station_id <- 1:nrow(police_station_coord_df)
+police_station_id <- dplyr::left_join(police_station_id, police_station_coord_df, by='police_station_id')
+
+## Convert coordinates to geojson for creating Maps on Superset
+
+get_police_station_geo_json <- function(station_id){
+ geography_df <- police_station_id[police_station_id$police_station_id == station_id,]
+ geojson_col <- suppressWarnings(geojsonio::geojson_json(input = geography_df, lat = 'lat', lon = 'lon')) 
+ return(as.character(geojson_col))
+}
+
+all_geojson <- lapply(police_station_id$police_station_id, get_police_station_geo_json) %>% unlist()
+police_station_id$geojson <- all_geojson
+
+# Joining the master dataset of police stations back with the meta dataset
+
+case_fir_df <- dplyr::left_join(case_fir_df, police_station_id[,c(-1)], by='police_station_id')
+
+# Exporting datasets - Individual datasets for Case Meta and Case History
+
+data.table::fwrite(case_fir_df, "datasets/Maharashtra/processed/mh_pocso_01.csv")
+data.table::fwrite(case_history_df, "datasets/Maharashtra/processed/mh_pocso_case_history_02.csv")
